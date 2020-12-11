a3776c889a7b7a505d38c9d7274a294a652c5d83
debjyotiC <djbabai.debjyoti@gmail.com>
2020-12-11 09:50:12 +0530

    added CSV writer (11-12-2020)

--- Module-2/SerialCom.py
+++ Module-2/SerialCom.py
@@ -1,9 +1,11 @@
 import time
 import serial
 import numpy as np
+import pandas as pd
 
 baudRate = 9600
-serialPort = '/dev/cu.usbmodem14201' # COM3
+# serialPort = '/dev/cu.usbmodem14201' #MacOS
+serialPort = 'COM3'  # Win10
 
 ser = serial.Serial(serialPort, baudRate)
 sensor_data = []
@@ -14,7 +16,11 @@ for itr in range(0, 10):
     sensor_data.append(data)
     data_point.append(itr)
     print("The data at {sample} is {data_sen}".format(sample=itr, data_sen=data))
-    time.sleep(1)    # get 1s delay
+    time.sleep(1)  # get 1s delay
 
 avg = np.average(sensor_data)
 print("The avg. of all data is: ", avg)
+
+values = {'Serial': data_point, 'Data': sensor_data}
+df_w = pd.DataFrame(values, columns=['Serial', 'Data'])
+df_w.to_csv("results.csv", index=None, header=True)
