64d0fe3ac13bef9f04169d90ea63e294718d4be0
debjyotiC <djbabai.debjyoti@gmail.com>
2021-01-22 11:00:05 +0530

    flask server code added

--- Module2/iot-server.py
+++ Module2/iot-server.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
-api = Api(app)
-app.debug = True
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 @app.route('/update', methods=['GET'])
@@ -11,10 +14,13 @@ def update():
     parser = reqparse.RequestParser()
     parser.add_argument('api_key', type=str)
     parser.add_argument('field1', type=float)
+
     data = parser.parse_args()
+
     print(data)
-    return '200'
+
+    return ''
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='192.168.0.107')
