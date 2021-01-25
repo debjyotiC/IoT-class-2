b53d555c43b58299f9e92b2c70791c7b580fb89b
debjyotiC <djbabai.debjyoti@gmail.com>
2021-01-25 08:57:54 +0530

    mongoDB handle added

--- Module2/iot-server.py
+++ Module2/iot-server.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
+import datetime
+import pymongo
 
 app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["test_DB"]
+mycol = mydb["sensor_data"]
 
 
 @app.route('/')
@@ -16,10 +22,12 @@ def update():
     parser.add_argument('field1', type=float)
 
     data = parser.parse_args()
+    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+    data['date'] = date
     print(data)
-
-    return ''
+    mycol.insert_one(data)
+    return '200 OK'
 
 
 if __name__ == '__main__':
